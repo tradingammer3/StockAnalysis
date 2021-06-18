c731645261ac0b6cb130826ee996f1998318795f
Aamerali Shadiqali Sayyed <Aamerali.Sayyed@jmcprojects.com>
2021-06-18 23:40:41 +0530

    combined all csv
    combined all csv

--- FinancialQuarterly.py
+++ FinancialQuarterly.py
@@ -12,8 +12,16 @@ Created on Fri Jun 18 17:20:10 2021
 # Please report bug/issues in the Q&A section
 # =============================================================================
 import pandas as pd 
-
 from enum import Enum
+import StockPeers as peers
+import os
+import glob
+
+
+
+stock = 'AAPL'
+stockCompetitors = peers.StockPeers(stock)
+stockCompetitorsList = peers.StockPeers.stockPeersList(stockCompetitors)
 
 tickers=["BA"] #list of tickers whose data needs to be extracted
 path = "F:\\Stockrow\\Income Statements" # please create this folder in your local machine
@@ -65,7 +73,8 @@ def Download_data(ticker,Fin,Term):
 
     df= pd.read_excel(URL)
     
-    #
+    
+    
     df.to_csv(filename,index=False)
     pd.read_csv(filename, header=None).T.to_csv(filename, header=False, index=False)
     
@@ -75,17 +84,31 @@ def Download_data(ticker,Fin,Term):
      # change the name of the columns 
     columns=df.columns.values.tolist()
     columns[0]="date"
+    columns[1]="stock"
     df.columns=columns
     
     #shorten the date and change the index to date
     df['date'] =pd.to_datetime(df["date"])
     df['date']  = df['date'].dt.date
+    df['stock'] = ticker
+    
     # save the file
     df.to_csv(filename,index=False)
 
 
-for ticker in tickers:
+
+# for ticker in tickers:
+for ticker in stockCompetitorsList:
     Download_data(ticker ,Financials.Balanced_Sheet,Terms.Annual)
     Download_data(ticker ,Financials.Balanced_Sheet,Terms.Quarterly)
     Download_data(ticker ,Financials.Balanced_Sheet,Terms.Trailing)
+
+
+
+os.chdir("F:\Stockrow\Income Statements\Annual")
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+
 
