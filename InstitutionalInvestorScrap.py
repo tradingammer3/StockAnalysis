ccf650f70a56c24980a5e2ef97ca665993f44d73
Aamerali Shadiqali Sayyed <Aamerali.Sayyed@jmcprojects.com>
2021-06-24 02:44:33 +0530

    Updated
    Updated

--- InstitutionalInvestorScrap.py
+++ InstitutionalInvestorScrap.py
@@ -3,49 +3,55 @@ from bs4 import BeautifulSoup
 import StockPeers as peers
 
 
+
 class InstitutionalInvestor():
 
         def __init__(self ):
             pass
         
+        
         def getInstitutionalInvestor(self , stockList):
             
-            identify = 0
+           
+            # identify = 0
             count_down = 0
             count_up = 0
             total_shares_exchanged = 0
             insider = pd.DataFrame()
+ 
             
-            for company in ['AAPL']:
-                insider1 = pd.read_html(f'http://openinsider.com/screener?s={company}&o=&pl=&ph=&ll=&lh=&fd=730&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=500&page=1')
-                insider1 = insider1[-3]
-                insider1['company'] = company
-                insider1.rename(columns = {"FilingÂ Date":"Fil Date", "TradeÂ Date":"Trade Date"}, inplace = True)
-                insider = pd.concat([insider,insider1])
-
+            # for company in ['AAPL', 'AMZN']:
+            insider1 = pd.read_html(f'http://openinsider.com/screener?s={stockList}&o=&pl=&ph=&ll=&lh=&fd=730&fdr=&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=500&page=1')
+            insider1 = insider1[-3]
+            insider1['company'] = stockList
+            insider = pd.concat([insider,insider1])
+  
+               
             
+                
+            insider.columns.values[1] = "Filing Date"
+            insider.columns.values[2] = "Trade Date"
+            insider.columns.values[4] = "Insider Name"
+            insider.columns.values[6] = "Trade Type" 
+            insider.columns.values[10] = "Percentage Own"  
             insider.to_csv('insider.csv')
-                
-                
-                
-
-            #         identify = identify + 1                    
-            #         if insider1.iloc[index , 3] > '2021-02-11':
-            #             if insider1['Qty'][index] > 0:
-            #                 count_up = count_up + 1
-            #             if insider1['Qty'][index] < 0:
-            #                 count_down = count_down + 1        
-                    
-            #         total_shares_exchanged = total_shares_exchanged + insider1['Qty'][index]
-                    
             
-            # insider1['Buy'] = count_up
-            # insider1['Sell'] = count_down
-            # insider1['Total'] = count_up + count_down
-            # insider1['ShareExchanged'] = total_shares_exchanged
             
-            # insider = pd.concat([insider,insider1])
-            return insider
+            for i in range(len(insider)):
+                if insider.iloc[i , 2] > '2021-02-11':
+                    if int(str(insider.iloc[i , 8]).replace(',' , '')) > 0:
+                        count_up = count_up + 1
+                    if int(str(insider.iloc[i , 8]).replace(',' , '')) < 0:
+                        count_down = count_down + 1  
+                    total_shares_exchanged = total_shares_exchanged + int(str(insider.iloc[i , 8]).replace(',' , ''))
+            
+                    insider['Buy'] = count_up
+                    insider['Sell'] = count_down
+                    insider['Total'] = count_up + count_down
+                    insider['ShareExchanged'] = total_shares_exchanged
+            
+            
+            return insider[['company' , 'Buy' , 'Sell' , 'Total' , 'ShareExchanged']]
             
             
 stockCompetitors = peers.StockPeers('AAPL')
@@ -53,6 +59,15 @@ stockCompetitorsList = peers.StockPeers.stockPeersList(stockCompetitors)
 
 
 getInstActivity = InstitutionalInvestor()
-getInstitutionsInvestorList = InstitutionalInvestor.getInstitutionalInvestor(getInstActivity , stockCompetitorsList)
 
+getInstitutionsInvestorList = pd.DataFrame()
+getInstitutionsInvestorListAll = pd.DataFrame()
+
+for stock in ['AAPL' , 'AMZN']:
+    getInstitutionsInvestorList = InstitutionalInvestor.getInstitutionalInvestor(getInstActivity , stock)
+    getInstitutionsInvestorListAll = pd.concat([getInstitutionsInvestorList , getInstitutionsInvestorListAll])
+
+
+getInstitutionsInvestorListAll = getInstitutionsInvestorListAll.drop_duplicates(
+  subset = ['company'], keep = 'last').reset_index(drop = True)
 
